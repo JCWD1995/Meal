a623f41cb28facecf7915858b6a78c77741a5215
wdole <wdole@192.168.28.135>
2018-03-03 19:49:27 -0500

    Initial Commit

---
+++ src/meal/Meal.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package meal;
+import java.util.Scanner;
+
+/**
+ *
+ * @author wdole
+ */
+public class Meal {
+    
+    double mealPrice;
+    int tipAmount, tax;
+    
+    public Meal() {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("What was the total of the meal's bill?");
+            mealPrice = sc.nextDouble();
+            System.out.println("What percent would you like to tip?");
+            tipAmount = sc.nextInt();
+            System.out.println("What is your state's tax rate?");
+            tax = sc.nextInt();
+        }
+    }
+    
+    public int totalPrice() {
+        final int percent = 100;
+        final int plusPercent = 1;
+        double total = mealPrice * (plusPercent + (tipAmount / percent)) * (plusPercent + (tax / percent));
+        return (int) Math.round(total);
+    }
+    
+    public static void main(String[] args) {
+        Meal current = new Meal();
+        System.out.println("Your meal's total comes to $" + current.totalPrice() + " after tip and tax.");
+    }
+    
+}
